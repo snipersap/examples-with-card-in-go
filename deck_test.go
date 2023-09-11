ccd43a0b9293a1b6aecb575806da2677809cc03e
snipersap <snipersap@gmail.com>
2023-09-11 15:04:50 +0200

    tests for first and last card in deck

--- deck_test.go
+++ deck_test.go
@@ -10,4 +10,10 @@ func TestNewDeck(t *testing.T){
 	if len(testDeck) != 20 {
 		t.Errorf("Size of deck not correct. Expected 20, but got %v",len(testDeck))
 	}
+	if testDeck[0] != "Ace of Hearts" {
+		t.Errorf("Expected Ace of Hearts as first card, but got %v",testDeck[0])
+	}
+	if(testDeck[len(testDeck)-1] != "Four of Clubs") {
+		t.Errorf("Expected the last card to be Four of Clubs, but got %v",testDeck[len(testDeck)-1])
+	}
 }
