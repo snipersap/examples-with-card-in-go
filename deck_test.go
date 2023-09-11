f9410f5a51eebbda0680a16311790ff0167548ed
snipersap <snipersap@gmail.com>
2023-09-11 14:52:51 +0200

    added length test of a new deck

---
+++ deck_test.go
@@ -0,0 +1,9 @@
+package main
+
+func TestNewDeck(t *testing.T){
+	testDeck := newDeck()
+
+	if len(testDeck) != 16 {
+		t.Errorf("Size of deck not correct. Expected 16, but got %v",len(testDeck))
+	}
+}
