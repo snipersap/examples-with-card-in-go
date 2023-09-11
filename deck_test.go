ab03e3881bafda727ddbc5b1cf44597a865f599a
snipersap <snipersap@gmail.com>
2023-09-11 14:55:39 +0200

    add failing test for deck size

--- deck_test.go
+++ deck_test.go
@@ -7,4 +7,7 @@ func TestNewDeck(t *testing.T){
 	if len(testDeck) != 16 {
 		t.Errorf("Size of deck not correct. Expected 16, but got %v",len(testDeck))
 	}
+	if len(testDeck) != 20 {
+		t.Errorf("Size of deck not correct. Expected 20, but got %v",len(testDeck))
+	}
 }
