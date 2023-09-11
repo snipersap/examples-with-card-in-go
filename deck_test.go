886f83eb79402c36a4b4627c7131e82350a16ccf
snipersap <snipersap@gmail.com>
2023-09-11 15:06:30 +0200

    commented errorneous test

--- deck_test.go
+++ deck_test.go
@@ -7,9 +7,9 @@ func TestNewDeck(t *testing.T){
 	if len(testDeck) != 16 {
 		t.Errorf("Size of deck not correct. Expected 16, but got %v",len(testDeck))
 	}
-	if len(testDeck) != 20 {
-		t.Errorf("Size of deck not correct. Expected 20, but got %v",len(testDeck))
-	}
+	// if len(testDeck) != 20 {
+	// 	t.Errorf("Size of deck not correct. Expected 20, but got %v",len(testDeck))
+	// }
 	if testDeck[0] != "Ace of Hearts" {
 		t.Errorf("Expected Ace of Hearts as first card, but got %v",testDeck[0])
 	}
